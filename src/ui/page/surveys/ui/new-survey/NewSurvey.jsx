60c06a69924c3bd482aa63c5fcdeab177d615e84
Anar Mamedov <anar26271@gmail.com>
2023-10-20 16:05:40 +0300

    test

--- src/ui/page/surveys/ui/new-survey/NewSurvey.jsx
+++ src/ui/page/surveys/ui/new-survey/NewSurvey.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Form, Select, Button, Table, ColorPicker, Space, Col, Row } from "antd";
 import { DeleteFilled, EditFilled } from "@ant-design/icons";
 import ThematicMapPage from "../map/ThematicMapPage";
@@ -10,6 +10,7 @@ export default function NewSurvey() {
   const [currentColor, setCurrentColor] = useState("#000000");
   const [currentText, setCurrentText] = useState("");
   const [validationError, setValidationError] = useState({ text: false, color: false });
+  const [isMinimized, setIsMinimized] = useState(window.innerWidth <= 768); // Assuming 768px as the breakpoint for mobile screens
 
   const btnStyle = {
     backgroundColor: "yellow",
@@ -42,111 +43,237 @@ export default function NewSurvey() {
 
   if (!formData) return <div>Loading...</div>;
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMinimized(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Row style={{ padding: "0px 0px", position: "absolute", top: "50px", width: "100%" }}>
-      <Col span={6} style={{ padding: "0px 0px 0px 20px", width: "500px", display: "flex", flexDirection: "column" }}>
-        <h2>{formData.title}</h2>
-        <Form>
-          {formData.fields.map((field) => {
-            switch (field.type) {
-              case "input-text":
-                return (
-                  <Form.Item
-                    style={formItemStyle}
-                    label={field.label}
-                    required={field.required}
-                    labelCol={{ span: 24, style: labelStyle }}
-                    wrapperCol={{ span: 24 }}>
-                    <Input placeholder={field.placeholder} />
-                  </Form.Item>
-                );
-              case "textarea":
-                return (
-                  <Form.Item
-                    style={formItemStyle}
-                    label={field.label}
-                    required={field.required}
-                    labelCol={{ span: 24, style: labelStyle }}
-                    wrapperCol={{ span: 24 }}>
-                    <Input.TextArea placeholder={field.placeholder} />
-                  </Form.Item>
-                );
-              case "select":
-                return (
-                  <Form.Item
-                    style={formItemStyle}
-                    label={field.label}
-                    required={field.required}
-                    labelCol={{ span: 24, style: labelStyle }}
-                    wrapperCol={{ span: 24 }}>
-                    <Select>
-                      {field.data.map((option) => (
-                        <Select.Option key={option.value} value={option.value}>
-                          {option.label}
-                        </Select.Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                );
-              case "SpecialComponent1":
-                return (
-                  <div key={field.field}>
-                    <Form.Item
-                      style={formItemStyle}
-                      label={field.label}
-                      labelCol={{ span: 24, style: labelStyle }}
-                      wrapperCol={{ span: 24 }}>
-                      <div style={{ display: "flex", gap: "10px" }}>
-                        <Input
-                          value={currentText}
-                          onChange={(e) => setCurrentText(e.target.value)}
-                          placeholder="Enter name"
-                          style={{ width: "50%", borderColor: validationError.text ? "red" : undefined }}
+      {isMinimized ? (
+        <>
+          <Col span={24} style={{ height: "300px", padding: "10px" }}>
+            <ThematicMapPage />
+          </Col>
+          <Col
+            span={24}
+            style={{ overflowY: "scroll", maxHeight: "calc(100vh - 410px)", padding: "0px 20px 70px 20px" }}>
+            <h2>{formData.title}</h2>
+            <Form>
+              {formData.fields.map((field) => {
+                switch (field.type) {
+                  case "input-text":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input placeholder={field.placeholder} />
+                      </Form.Item>
+                    );
+                  case "textarea":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input.TextArea placeholder={field.placeholder} />
+                      </Form.Item>
+                    );
+                  case "select":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Select>
+                          {field.data.map((option) => (
+                            <Select.Option key={option.value} value={option.value}>
+                              {option.label}
+                            </Select.Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    );
+                  case "SpecialComponent1":
+                    return (
+                      <div key={field.field}>
+                        <Form.Item
+                          style={formItemStyle}
+                          label={field.label}
+                          labelCol={{ span: 24, style: labelStyle }}
+                          wrapperCol={{ span: 24 }}>
+                          <div style={{ display: "flex", gap: "10px" }}>
+                            <Input
+                              value={currentText}
+                              onChange={(e) => setCurrentText(e.target.value)}
+                              placeholder="Enter name"
+                              style={{ width: "50%", borderColor: validationError.text ? "red" : undefined }}
+                            />
+
+                            <ColorPicker value={currentColor} onChange={(color) => setCurrentColor(color)}>
+                              <Button type="primary" style={btnStyle}>
+                                Choose Color
+                              </Button>
+                            </ColorPicker>
+
+                            <Button onClick={handleAddToList}>Add to List</Button>
+                          </div>
+                        </Form.Item>
+                        <Table
+                          dataSource={answers}
+                          columns={[
+                            {
+                              title: "Color",
+                              dataIndex: "color",
+                              key: "color",
+                              render: (color) => <span style={{ color: color }}>{color}</span>,
+                            },
+                            { title: "Answers", dataIndex: "text", key: "text" },
+                            {
+                              title: "Operations",
+                              key: "operations",
+                              render: () => (
+                                <div style={{ display: "flex", gap: "10px" }}>
+                                  <DeleteFilled />
+                                  <EditFilled />
+                                </div>
+                              ),
+                            },
+                          ]}
                         />
-
-                        <ColorPicker value={currentColor} onChange={(color) => setCurrentColor(color)}>
-                          <Button type="primary" style={btnStyle}>
-                            Choose Color
-                          </Button>
-                        </ColorPicker>
-
-                        <Button onClick={handleAddToList}>Add to List</Button>
+                        <Button style={{ float: "right", marginTop: "10px" }}>Save</Button>
                       </div>
-                    </Form.Item>
-                    <Table
-                      dataSource={answers}
-                      columns={[
-                        {
-                          title: "Color",
-                          dataIndex: "color",
-                          key: "color",
-                          render: (color) => <span style={{ color: color }}>{color}</span>,
-                        },
-                        { title: "Answers", dataIndex: "text", key: "text" },
-                        {
-                          title: "Operations",
-                          key: "operations",
-                          render: () => (
-                            <div style={{ display: "flex", gap: "10px" }}>
-                              <DeleteFilled />
-                              <EditFilled />
-                            </div>
-                          ),
-                        },
-                      ]}
-                    />
-                    <Button style={{ float: "right", marginTop: "10px" }}>Save</Button>
-                  </div>
-                );
-              default:
-                return null;
-            }
-          })}
-        </Form>
-      </Col>
-      <Col span={18} style={{ width: "100%", height: "90vh", padding: "10px" }}>
-        <ThematicMapPage />
-      </Col>
+                    );
+                  default:
+                    return null;
+                }
+              })}
+            </Form>
+          </Col>
+        </>
+      ) : (
+        <>
+          <Col
+            span={6}
+            style={{ padding: "0px 0px 0px 20px", width: "500px", display: "flex", flexDirection: "column" }}>
+            <h2>{formData.title}</h2>
+            <Form>
+              {formData.fields.map((field) => {
+                switch (field.type) {
+                  case "input-text":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input placeholder={field.placeholder} />
+                      </Form.Item>
+                    );
+                  case "textarea":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input.TextArea placeholder={field.placeholder} />
+                      </Form.Item>
+                    );
+                  case "select":
+                    return (
+                      <Form.Item
+                        style={formItemStyle}
+                        label={field.label}
+                        required={field.required}
+                        labelCol={{ span: 24, style: labelStyle }}
+                        wrapperCol={{ span: 24 }}>
+                        <Select>
+                          {field.data.map((option) => (
+                            <Select.Option key={option.value} value={option.value}>
+                              {option.label}
+                            </Select.Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    );
+                  case "SpecialComponent1":
+                    return (
+                      <div key={field.field}>
+                        <Form.Item
+                          style={formItemStyle}
+                          label={field.label}
+                          labelCol={{ span: 24, style: labelStyle }}
+                          wrapperCol={{ span: 24 }}>
+                          <div style={{ display: "flex", gap: "10px" }}>
+                            <Input
+                              value={currentText}
+                              onChange={(e) => setCurrentText(e.target.value)}
+                              placeholder="Enter name"
+                              style={{ width: "50%", borderColor: validationError.text ? "red" : undefined }}
+                            />
+
+                            <ColorPicker value={currentColor} onChange={(color) => setCurrentColor(color)}>
+                              <Button type="primary" style={btnStyle}>
+                                Choose Color
+                              </Button>
+                            </ColorPicker>
+
+                            <Button onClick={handleAddToList}>Add to List</Button>
+                          </div>
+                        </Form.Item>
+                        <Table
+                          dataSource={answers}
+                          columns={[
+                            {
+                              title: "Color",
+                              dataIndex: "color",
+                              key: "color",
+                              render: (color) => <span style={{ color: color }}>{color}</span>,
+                            },
+                            { title: "Answers", dataIndex: "text", key: "text" },
+                            {
+                              title: "Operations",
+                              key: "operations",
+                              render: () => (
+                                <div style={{ display: "flex", gap: "10px" }}>
+                                  <DeleteFilled />
+                                  <EditFilled />
+                                </div>
+                              ),
+                            },
+                          ]}
+                        />
+                        <Button style={{ float: "right", marginTop: "10px" }}>Save</Button>
+                      </div>
+                    );
+                  default:
+                    return null;
+                }
+              })}
+            </Form>
+          </Col>
+          <Col span={18} style={{ width: "100%", height: "90vh", padding: "10px" }}>
+            <ThematicMapPage />
+          </Col>
+        </>
+      )}
     </Row>
   );
 }
